070e05a25020257e97852dbfd0678a47707d651b
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2021-05-04 21:12:44 +0900

    ✨ add click event to button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 const App = () => {
+  const [state, setState] = useState('');
+  const onClick=useCallback(()=>{
+    setState('clicked')
+  },[]);
   return useMemo(() =>
     <div className="container">
       <img width="100%" src="https://i.postimg.cc/9MFvz25j/movingtogether-primary-804744d5605a360dc6a2faca9183c51f.jpg" alt="" />
-      <button>Download</button>
+      <button onClick={()=>onClick()}>Download</button>
     </div>, []);
 }
 
