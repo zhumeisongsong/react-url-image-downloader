8133998300b694567c15e6bbe392bd01ceff4da9
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2021-05-04 19:39:34 +0900

    ✨ add test coverage & snapshots

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -7,11 +6,20 @@ import App from './App';
 configure({ adapter: new Adapter() });
 
 describe('App component', function() {
-  it('should render img tag without throwing an error', function() {
+  it('should test App component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+   });
+
+   it('should render App component', () => {
+    expect(shallow(<App />).find('.container').length).toEqual(1);
+   });
+
+  it('should render img tag', function() {
     expect(shallow(<App />).find('img').length).toEqual(1);
   });
 
-  it('should render button tag without throwing an error', function() {
+  it('should render button tag', function() {
     expect(shallow(<App />).find('button').length).toEqual(1);
   });
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useMemo } from 'react';
 
 const App = () => {
-  return useMemo(() => <div className="App">
-    <img width="100%" src="https://i.postimg.cc/9MFvz25j/movingtogether-primary-804744d5605a360dc6a2faca9183c51f.jpg" alt="" />
-    <button>Download</button>
-  </div>, []);
+  return useMemo(() =>
+    <div className="container">
+      <img width="100%" src="https://i.postimg.cc/9MFvz25j/movingtogether-primary-804744d5605a360dc6a2faca9183c51f.jpg" alt="" />
+      <button>Download</button>
+    </div>, []);
 }
 
 export default App;
